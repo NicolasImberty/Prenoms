4f86ffeed958588a223c3bd9fc18009dc132b1b9
Nicolas IMBERTY <45438396+NicolasImberty@users.noreply.github.com>
2019-07-07 19:10:47 +0200

    Update ui.R

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,1 +1,16 @@
+# interface utilisateur
+# déclaration des entrées
+# structure de la page, placement des sorties
 
+dashboardPage(skin = "purple",
+              
+              # barre en tête
+              source("src/ui/01_ui_header.R", local = TRUE)$value,
+              
+              # barre de côté
+              source("src/ui/02_ui_sidebar.R", local = TRUE)$value,
+              
+              # corps
+              source("src/ui/03_ui_body.R", local = TRUE)$value
+
+)
