7f0f07435aad4ddd8c2377c6a6c080d1bc46e660
Nicolas IMBERTY <45438396+NicolasImberty@users.noreply.github.com>
2019-07-07 19:10:07 +0200

    Update server.R

--- shiny/server.R
+++ shiny/server.R
@@ -1,1 +1,14 @@
+# serveur
+# déclaration et calcul des sorties
 
+shinyServer(function(input, output, session) {
+  
+  source("src/server/31_server_evo_ge.R", local = TRUE)
+  
+  source("src/server/32_server_evo_pre.R", local = TRUE)
+  
+  source("src/server/33_server_top.R", local = TRUE)
+  
+  source("src/server/34_server_liste.R", local = TRUE)
+  
+})
