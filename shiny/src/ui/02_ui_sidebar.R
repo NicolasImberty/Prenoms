14db98aede92d88429e0cea6398183c0092053f9
Nicolas IMBERTY <45438396+NicolasImberty@users.noreply.github.com>
2019-07-08 20:04:10 +0200

    Create 02_ui_sidebar.R

---
+++ shiny/src/ui/02_ui_sidebar.R
@@ -0,0 +1,34 @@
+dashboardSidebar(
+  sidebarMenu(
+    menuItem(
+      "Evolution générale",
+      tabName = "evo_ge",
+      icon = icon("bar-chart")
+    ),
+    menuItem(
+      "Evolution par prénom",
+      tabName = "evo_pre",
+      icon = icon("dashboard")
+    ),
+    menuItem(
+      "Top",
+      tabName = "top",
+      icon = icon("signal")
+    ),
+    menuItem(
+      "Liste",
+      tabName = "liste",
+      icon = icon("th-list")
+    ),
+    menuItem(
+      "Département",
+      tabName = "dpt",
+      icon = icon("map-o")
+    ),
+    menuItem(
+      "source",
+      icon = icon("file-code-o"),
+      href = "https://www.data.gouv.fr/fr/datasets/ficher-des-prenoms-de-1900-a-2017/"
+    )
+  )
+)
