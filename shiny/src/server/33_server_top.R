0090901c59129f7fe19517adbaa8517b61b10d45
Nicolas IMBERTY <45438396+NicolasImberty@users.noreply.github.com>
2019-09-29 20:58:15 +0200

    Delete 33_server_top.R

--- shiny/src/server/33_server_top.R
+++
@@ -1,6 +0,0 @@
-output$output_top <- renderTable({
-  m1 <- input$input_periode[1]
-  m2 <- input$input_periode[2]
-  
-  liste_prenoms[annee >= m1 & annee <= m2, .(nombre = sum(nombre)), by = prenom][order(-nombre)][1:10]
-})
